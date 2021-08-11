cd9639ac3ee01339693cbc8617cbe7ac4161e4de
David Rojo <davidrojo1997@gmail.com>
2021-08-11 04:14:47 +0200

    first route

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
-app.set('port', 3000)
+const path = require('path')
 
+app.set('port', process.env.PORT)
+
+// ROUTES
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'))
+})
+// Static files
+
+// Listening server
 app.listen(app.get('port'), () => {
   console.log(`Server on port ${app.get('port')}`)
 })
